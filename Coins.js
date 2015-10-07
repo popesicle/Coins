b3dd53926e767b99e6427ba4dae3d7d12e097c80
Tyler Neustaedter <rtneust@gmail.com>
2015-10-07 10:20:24 -0500

    fixed my stuff.

--- Coins.js
+++ Coins.js
@@ -25,12 +25,13 @@ function coins(sum){
 
 		while(sum > 0){
 			coinArray.map(function(coin){
-				if(sum >= coin){
+				while(sum >= coin){
 					sum = sum - coin
 					coinMoney.push(coin)
 				}
 			})
-			sum - 1
+			console.log(sum)
+			console.log(coinMoney)
 		}
 		return coinMoney
 		
@@ -46,3 +47,27 @@ console.assert( result[0] === 10 && result[1] === 1 )
 result = coins(39)
 // --> 25, 10, 1, 1, 1, 1
 console.assert( result[0] === 25 && result[1] === 10 && result[2] === 1 && result[3] === 1 && result[4] === 1 && result[5] === 1)
+
+
+function coins(sum){
+
+	var coinArray = [25, 10, 5, 1],
+		coinMoney = []
+
+
+		while(sum > 0){
+			coinArray.forEach(function(coin){
+				while(sum >= coin){
+					sum = sum - coin
+					coinMoney.push(coin)
+				}
+			})
+		}
+		return coinMoney
+		
+}
+
+
+
+
+
